4b26c35dbf5f176159410c3280605e9b79f9eda7
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-01-18 15:34:42 +0530

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Helloworld{
+	public static void main(String[] args){
+		int i=1;
+		int j=1;	
+		for(i =1;i<=10;i++){
+			if(i%2==0){
+				System.out.println(i+" is Even");
+			}
+			else{
+				System.out.println(i+" is Odd");
+			}
+		}
+		
+
+	}
+}
+
+
